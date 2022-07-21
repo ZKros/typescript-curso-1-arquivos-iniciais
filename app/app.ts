3fe9c5c447d04f4774921020348a7af15eabfce6
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-07-21 12:40:47 -0300

    Projeto Alura

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import { NegocicaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegocicaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	controller.adicionar();
+})
+
+
